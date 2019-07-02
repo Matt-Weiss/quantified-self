23b3636b6b19dcbc55d857b6aa7d3765b3d1ab84
Matt Weiss <weiss.matt@outlook.com>
2019-07-02 16:49:44 -0600

    Adds GET /:id route. Changes function names to be more precise
    Co-authored by: Jalena Taylor 
    <45905026+jalena-penaligon@users.noreply.github.com>

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -2,15 +2,39 @@ var express = require('express');
 var router = express.Router();
 var Food = require('../../../models').Food;
 
-const existingFood = (foodName) => {
+const existingFoodByName = (foodName) => {
   return Food.findOne({ where: {name: foodName.toLowerCase()}})
     .then(food => {
-      if (food != null) { return true } else { return false }
+      if (food != null) { return food } else { return false }
     })
 }
 
+const existingFoodById = (foodId) => {
+  return Food.findOne({ where: {id: foodId}})
+  .then(food => {
+    if (food != null) { return food } else { return false }
+  })
+}
+
+router.get("/:id", function(req, res, next) {
+  var itExists = existingFoodById(req.params.id)
+  .then(foodExists => {
+    if (foodExists == false) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(404).send("Food not found");
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(foodExists));
+    }
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({error})
+  });
+});
+
 router.post("/", function (req, res, next) {
-  var itExists = existingFood(req.body.food.name)
+  var itExists = existingFoodByName(req.body.food.name)
   .then(itemExists => {
     if (itemExists == false) {
       Food.create({name: req.body.food.name.toLowerCase(), calories: req.body.food.calories})
