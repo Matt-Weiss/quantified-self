297646c62bd4440d8d786c5e401ad45fbb134e89
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-02 16:46:33 -0600

    Adds before and after actions to test
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- tests/models/food.spec.js
+++ tests/models/food.spec.js
@@ -3,14 +3,25 @@ var request = require("supertest");
 var app = require('../../app');
 var Food = require('../../models').Food;
 
-describe('Food model', () => {
-  test('Is created with attributes', () => {
-    Food.create({
-        name: "Apple",
-        calories: 120
-    }).then(food =>{
-      expect(food.name).toBe("Apple")
-      expect(food.calories).toBe(120)
+describe('api', () => {
+  beforeAll(() => {
+    shell.exec('npx sequelize db:create')
+  });
+  beforeEach(() => {
+    shell.exec('npx sequelize db:migrate')
+  });
+  afterEach(() => {
+    // shell.exec('npx sequelize db:migrate:undo:all')
+  });
+  describe('Food model', () => {
+    test('Is created with attributes', () => {
+      Food.create({
+          name: "Apple",
+          calories: 120
+      }).then(food =>{
+        expect(food.name).toBe("Apple")
+        expect(food.calories).toBe(120)
+      })
     })
-  })
-});
+  });
+})
