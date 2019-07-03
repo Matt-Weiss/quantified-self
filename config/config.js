ce8ad6f5c063a4046b11a27c09517bc43dccdf4a
Matt Weiss <weiss.matt@outlook.com>
2019-07-03 14:40:45 -0600

    Additional changes for TravisCI
    Co-authored by: Jalena Taylor 
    <45905026+jalena-penaligon@users.noreply.github.com>

---
+++ config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+   development: {
+     username:  "mattweiss" ,
+     password: null,
+     database:  "quantified_development" ,
+     host:  "127.0.0.1" ,
+     dialect:  "postgres" ,
+     operatorsAliases: false
+  },
+   test: {
+     username: process.env.USERNAME ,
+     password: null,
+     database:  "quantified_test" ,
+     host:  "127.0.0.1" ,
+     dialect:  "postgres" ,
+     operatorsAliases: false
+  },
+   production: {
+     username:  "mattweiss" ,
+     password: null,
+     database:  "quantified_production" ,
+     host:  "127.0.0.1" ,
+     dialect:  "postgres" ,
+     operatorsAliases: false,
+     use_env_variable:  DATABASE_URL
+  }
+}
