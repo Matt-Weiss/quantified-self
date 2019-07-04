c6f23e39e59bcc79176f346b8bc6b9163b0e0341
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-04 13:35:41 -0600

    Creates route to post a new food to a meal

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -4,7 +4,6 @@ var Meal = require('../../../models').Meal;
 var Food = require('../../../models').Food;
 var MealFoods = require('../../../models').MealFoods;
 
-
 const parseMeals = (meals) => {
    return meals.map(getMealObjects)
 }
@@ -35,6 +34,20 @@ const existingMealById = (mealId) => {
   return Meal.findOne({ where: {id: mealId}})
   .then(meal => {
     if (meal != null) { return meal } else { return false }
+  })
+}
+
+const existingFoodById = (foodId) => {
+  return Food.findOne({ where: {id: foodId}})
+  .then(food => {
+    if (food != null) { return food } else { return false }
+  })
+}
+
+const createMealFood = (mealId, foodId) => {
+  MealFoods.create({meal_id: mealId, food_id: foodId})
+  .then(mealFood => {
+    return mealFood.id
   })
 }
 
@@ -71,6 +84,31 @@ router.get("/:id/foods", function (req, res, next) {
     res.setHeader("Content-Type", "application/json");
     res.status(500).send({error})
   });
+})
+
+router.post("/:id/foods/:id", function (req, res, next) {
+  existingMealById(req.path.split("/")[1])
+  .then(mealExists => {
+    if (mealExists == false) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(404).send("Meal not found");
+    } else {
+      existingFoodById(req.params.id)
+      .then(foodExists => {
+        if (foodExists == false) {
+          res.setHeader("Content-Type", "application/json");
+          res.status(404).send("Food not found");
+        } else {
+          createMealFood(mealExists.id, foodExists.id)
+          res.setHeader("Content-Type", "application/json");
+          res.status(201).send({message: `Successfully added ${foodExists.name} to ${mealExists.name}`});
+        }
+      })}
+      })
+      .catch(error => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(500).send({error})
+      });
 })
 
 module.exports = router;
