15e8107a239a2322aadc457ba08ff7f013045825
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-09 13:31:44 -0600

    Adds CORS to app.js
    Co-authored-by: Matt Weiss <45211960+Matt-Weiss@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var foodsRouter = require('./routes/api/v1/foods');
@@ -12,6 +13,7 @@ var app = express();
 
 app.on('close', () => sequelize.close())
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
